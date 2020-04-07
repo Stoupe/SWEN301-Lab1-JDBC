91c232a685bf7f2c04a0bd876689ed065f432c9a
Stoupe <H.stoupe@gmail.com>
2020-04-07 15:29:01 +1200

    Add Data & General Setup

---
+++ src/swen301/jdbc/FetchStudents.java
@@ -0,0 +1,9 @@
+package swen301.jdbc;
+
+public class FetchStudents {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Done");
+    }
+
+}
---
+++ src/swen301/jdbc/package-info.java
@@ -0,0 +1,1 @@
+package swen301.jdbc;
